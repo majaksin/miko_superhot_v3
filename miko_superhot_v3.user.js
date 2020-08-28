8649cbc12a04c6d29a7c2dc3927ecc7209e3a431
majaksin <67994038+majaksin@users.noreply.github.com>
2020-08-28 23:16:39 +0900

    Update miko_superhot_v3.user.js

--- miko_superhot_v3.user.js
+++ miko_superhot_v3.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         한여름의 무녀들 (R-18)
+// @name         한여름의 소녀들 (R-18)
 // @namespace    miko_superhot_v3
 // @version      2.1.8
-// @description  이치히메와 마이가 수영복을 모두 탈의합니다.
+// @description  마이와 이치히메, 지니아가 수영복을 모두 탈의합니다.
 // @author       소사희+돌죽
 // @license      MIT
 // @icon         https://majaksin.github.io/miko_superhot_v3/preview.png
